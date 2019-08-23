97600de859cc4c674c5867e5a49c454bb1b4cdd4
Ian Hamilton <ian.hamilton@academytrainee.com>
2019-08-23 16:10:43 +0100

    eing

--- application/users/utils.py
+++ application/users/utils.py
@@ -1,5 +1,4 @@
 import os
-import secrets
 from PIL import Image
 from flask import url_for, current_app
 from flask_mail import Message
@@ -19,11 +18,11 @@ def save_picture(form_picture):
 	return picture_fn
 
 def send_reset_email(user):
-	token = user.get_reset_token() #expires_seconds=1800s default
+	#token = user.get_reset_token() #expires_seconds=1800s default
 	msg = Message('Password Reset Request', 
 			sender='blog.ianhamilton.noreply@gmail.com', 
 			recipients=[user.email])
 
-	msg.body = ''.join(["BLOG - PASSWORD RESET To reset your password, visit the following link: {url_for('users.reset_token', token=token, _external=True)} If you did not make this request, please ignore this email."])
+	msg.body = ''.join(["PASSWORD REST NOT SUPPORTED YET"])
 
 	mail.send(msg)
