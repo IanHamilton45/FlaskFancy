db057d344937680ad904d5f73b6164bb2a43cc14
Ian Hamilton <ian.hamilton@academytrainee.com>
2019-08-23 14:50:48 +0100

    wwfd

--- application/__init__.py
+++ application/__init__.py
@@ -3,10 +3,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-from application.config import Config
 
-db = SQLAlchemy()
-bcrypt = Bcrypt()
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:password@mysql:3306/blog'
+app.config['SECRET_KEY'] = '82fcc1d59465a9b274d13db8441cbc30'
+
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 login_manager = LoginManager()
 login_manager.login_view = 'users.login'
@@ -14,23 +18,6 @@ login_manager.login_message_category = 'info'
 
 mail = Mail()
 
-def create_app(config_class=Config):
-	app = Flask(__name__)
-	app.config.from_object(Config)
-
-	db.init_app(app)
-	bcrypt.init_app(app)
-	login_manager.init_app(app)
-	mail.init_app(app)
-
-	from application.users.routes import users
-	from application.posts.routes import posts
-	from application.main.routes import main
-	from application.errors.handlers import errors
-
-	app.register_blueprint(users)
-	app.register_blueprint(posts)
-	app.register_blueprint(main)
-	app.register_blueprint(errors)
-
-	return app
+from application.main import routes
+from application.users import routes
+from application.posts import routes
